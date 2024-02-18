03e1e2781bcaeeb608b0781744488751bd453b6b
Avery Maxwell <averymaxwell42@gmail.com>
2024-02-18 14:11:25 -0600

    Assign html inputs to variables

---
+++ app.js
@@ -0,0 +1,6 @@
+const nameInputField = document.querySelector('#name-input');
+const phoneInputField = document.querySelector('#num-input');
+const emailInputField = document.querySelector('#email-input');
+const messageInputField = document.querySelector('#message-input');
+const submitButton = document.querySelector('#submit-button');
+
